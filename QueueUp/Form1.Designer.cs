9b9e9cfc755453a1f1736b199cb67442de689b04
Degenerate <the.d3generate@gmail.com>
2016-01-26 00:23:07 -0500

    trying to fix git

---
+++ QueueUp/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 595);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Channel";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 592);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(432, 62);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(221, 553);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(659, 62);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(182, 553);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(222, 62);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(204, 553);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 62);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(204, 308);
+            this.textBox4.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(729, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "IRC Chat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(510, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "TwitchChat";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(281, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Current Queue";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Current Group";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 645);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ QueueUp/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public void ircthread()
+        {
+            int port;
+            string buf, nick, pw, server, chan, user, uname, msg;
+
+            System.Net.Sockets.TcpClient sock = new System.Net.Sockets.TcpClient();
+            System.IO.TextReader input;
+            System.IO.TextWriter output;
+
+            nick = "queueupchat";
+            pw = "oauth:mwhpwsgzsohz1ltvj9wtwnkwn0aqzq";
+            server = "irc.twitch.tv";
+            port = 6667;
+            chan = "#" + textBox1.Text.ToLower();
+            sock.Connect(server, port);
+
+            input = new System.IO.StreamReader(sock.GetStream());
+            output = new System.IO.StreamWriter(sock.GetStream());
+
+            //Starting USER and NICK login commands
+            output.Write(
+               "PASS " + pw + "\r\n" +
+               "NICK " + nick + "\r\n" +
+               "USER " + nick + " 8 * :" + nick + "\r\n" +   //this is
+               "CAP REQ :twitch.tv/membership" + "\r\n " +   //the hew magic
+               "JOIN " + chan + "\r\n"
+
+            );
+            output.Flush();
+
+            //Process each line received from irc server
+            for (buf = input.ReadLine(); ; buf = input.ReadLine())
+            {
+                if (buf.Contains("PRIVMSG"))
+                {
+                    string[] split = buf.Split(new Char[] { ':' });
+                    user = split[1];
+                    string[] unamesplit = user.Split(new Char[] { '!' });
+                    uname = unamesplit[0];
+                    msg = split[2];
+
+                    textBox2.Invoke((Action)delegate
+                    {
+
+                        int s = textBox2.Text.Length - textBox2.Height;
+                        if (s < 0) s = 0;
+                        textBox2.Text = textBox2.Text.Substring(s);
+                        textBox2.Text += DateTime.Now.ToShortTimeString() + " " + uname + ": " + msg + "\n\n";
+
+                        textBox2.Enabled = false;
+                        textBox2.WordWrap = true;
+                        textBox2.AcceptsReturn = true;
+                    });
+
+                    this.Invoke((Action)delegate
+                    {
+                        this.TopMost = true;
+
+                    });
+
+                }
+
+
+                //Send pong reply to any ping messages
+                if (buf.StartsWith("PING ")) { output.Write(buf.Replace("PING", "PONG") + "\r\n"); output.Flush(); }
+                if (buf[0] != ':') continue;
+
+                /* IRC commands come in one of these formats:
+                 * :NICK!USER@HOST COMMAND ARGS ... :DATA\r\n
+                 * :SERVER COMAND ARGS ... :DATA\r\n
+                 */
+
+                
+
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadStart ircth = new ThreadStart(ircthread);
+            Thread chatthread = new Thread(ircth);
+            chatthread.Start();
+
+            //set web browser here
+        }
+    }
+}
